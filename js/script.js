fe156a45d98f5c5f1db67c4d3d29c496f0b9de9e
Namrata Asnani <asnaninamrata29@gmail.com>
2024-01-19 11:13:08 +0530

    My Portfolio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*window.onload = function() {
+    document.getElementById('autoRedirect').click();*/
+    var typed = new Typed(".typing", {
+        strings:["","Web Designer","web Developer","Graphic Designer","Youtuber"],
+        typeSpeed:100,
+        BackSpeed:60,
+        loop:true
+    })
---
+++ js/style-switcher.js
@@ -0,0 +1,60 @@
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else
+        {
+            style.setAttribute("disabled","true");
+
+        }
+    });
+}
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else
+    {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
+/*const dayNight = document.querySelector(".day-night");
+
+dayNight.addEventListener("click", () => {
+    const icon = dayNight.querySelector("i");
+    icon.classList.toggle("fa-sun");
+    icon.classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+});
+
+window.addEventListener("load", () => {
+    const icon = dayNight.querySelector("i");
+    
+    if (document.body.classList.contains("dark")) {
+        icon.classList.add("fa-sun");
+    } else {
+        icon.classList.add("fa-moon");
+    }
+});*/
+
